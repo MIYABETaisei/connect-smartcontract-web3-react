8285c81f4123553f21e9dcf75e0d75be51d56c6b
MIYABETaisei <mytspe46@gmail.com>
2022-08-03 10:41:15 +0900

    ✨

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 import { useWeb3React } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
-import { abi } from "../constants/abi";
+import { abi } from "../../constants/abi";
 import { ethers } from "ethers";
 
 const injected = new InjectedConnector();
@@ -33,7 +32,7 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className={styles.container}>
+    <div>
       {active ? (
         "Connected! "
       ) : (
