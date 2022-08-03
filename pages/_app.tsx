95b261b8fc2cb18625b6c17389671474e5e24fc5
MIYABETaisei <mytspe46@gmail.com>
2022-08-03 10:38:34 +0900

    ✨

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Web3ReactProvider } from "@web3-react/core";
+import {
+  ExternalProvider,
+  JsonRpcFetchFunc,
+  Web3Provider,
+} from "@ethersproject/providers";
+
+const getLibrary = (provider: ExternalProvider | JsonRpcFetchFunc) => {
+  return new Web3Provider(provider);
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
